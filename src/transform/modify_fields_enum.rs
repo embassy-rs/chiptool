6509a8d68e775c2828e418527a5c53beec3cc609
Dion Dokter <dev@diondokter.nl>
2026-03-09 14:38:28 +0100

    Be lazy with finding enum id

--- src/transform/modify_fields_enum.rs
+++ src/transform/modify_fields_enum.rs
@@ -13,16 +13,23 @@ pub struct ModifyFieldsEnum {
 
 impl ModifyFieldsEnum {
     pub fn run(&self, ir: &mut IR) -> anyhow::Result<()> {
-        let matched_enums = match_all(ir.enums.keys().cloned(), &self.enumm);
-        if matched_enums.len() != 1 {
-            anyhow::bail!(
-                "Expected exactly one enum to match, found {}",
-                matched_enums.len()
-            );
-        }
-        let enum_id = matched_enums.first().unwrap().clone();
+        let mut enum_id = None;
 
         for id in match_all(ir.fieldsets.keys().cloned(), &self.fieldset) {
+            let enum_id = match &enum_id {
+                Some(enum_id) => enum_id,
+                None => {
+                    let matched_enums = match_all(ir.enums.keys().cloned(), &self.enumm);
+                    if matched_enums.len() != 1 {
+                        anyhow::bail!(
+                            "Expected exactly one enum to match, found {}",
+                            matched_enums.len()
+                        );
+                    }
+                    enum_id.insert(matched_enums.first().unwrap().clone())
+                }
+            };
+
             let fs = ir.fieldsets.get_mut(&id).unwrap();
             fs.fields
                 .iter_mut()
