096abd25d26964b0abb0118485262c63e014e31d
Alex Zepeda <github@inferiorhumanorgans.com>
2026-04-06 14:34:36 -0700

    gen_block: Resolve dependencies.
    This will:
    
    * Assume each YAML file has exactly one block
    * Assume he name of the YAML file is the unsanitized block name
    * Load additional YAML files as needed
    * Only garbage collect extra blocks (not other types).

--- src/commands/gen_block.rs
+++ src/commands/gen_block.rs
@@ -6,8 +6,8 @@ use crate::{
 
 use anyhow::Result;
 use clap::Parser;
-use std::fs;
 use std::path::PathBuf;
+use std::{collections::HashSet, fs};
 
 /// Generate Rust code from a YAML register block
 #[derive(Parser)]
@@ -25,6 +25,29 @@ pub struct GenBlock {
 pub fn gen_block(args: GenBlock) -> Result<()> {
     let data = fs::read(&args.input)?;
     let mut ir: IR = serde_yaml::from_slice(&data)?;
+    let block_name = args.input.with_extension("");
+    let block_name = block_name.file_name().unwrap().to_string_lossy();
+
+    let dependencies = ir
+        .blocks
+        .values()
+        .filter_map(|block| block.extends.clone())
+        .collect::<HashSet<_>>();
+
+    for dependency in dependencies.iter() {
+        let data = fs::read(args.input.with_file_name(dependency).with_extension("yaml"))?;
+        let sub_ir: IR = serde_yaml::from_slice(&data)?;
+        if dependencies.contains(sub_ir.blocks.keys().next().unwrap()) {
+            ir.merge(sub_ir);
+            break;
+        }
+    }
+
+    crate::transform::expand_extends::ExpandExtends {}
+        .run(&mut ir)
+        .unwrap();
+
+    ir.blocks.retain(|name, _block| *name == block_name);
 
     crate::transform::sanitize::Sanitize {}
         .run(&mut ir)
