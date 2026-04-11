ec6eec0492538e462611a766aa922fcebf765ee4
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2026-04-11 22:53:07 +0200

    Allow non_upper_case_globals
    Now we use CamelCase for enum variants, which triggers this lint for
    sparse enums that get generated as structs with associated consts.

--- src/generate/mod.rs
+++ src/generate/mod.rs
@@ -169,6 +169,7 @@ pub fn render(ir: &IR, opts: &Options) -> Result<TokenStream> {
     root.items.extend(quote!(
         #![allow(non_camel_case_types)]
         #![allow(non_snake_case)]
+        #![allow(non_upper_case_globals)]
         #![doc=#doc]
     ));
 
