93d51064dfcdf28cc758ef9d50b138f511f6a284
datdenkikniet <38322042+datdenkikniet@users.noreply.github.com>
2026-04-11 22:14:57 +0200

    Detect duplicate renames (#122)
    * Detect duplicate renamed registers
    
    * Detect renames when renaming fields
    
    * Also check for duplicate names when renaming interrupts and peripherals

--- src/transform/rename_peripherals.rs
+++ src/transform/rename_peripherals.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use super::common::*;
@@ -7,17 +9,31 @@ use crate::ir::*;
 pub struct RenamePeripherals {
     pub from: RegexSet,
     pub to: String,
+    #[serde(default = "get_true")]
+    pub error_on_duplicate: bool,
 }
 
 impl RenamePeripherals {
     pub fn run(&self, ir: &mut IR) -> anyhow::Result<()> {
-        for d in ir.devices.values_mut() {
+        let mut had_duplicate = false;
+
+        for (dev_name, d) in ir.devices.iter_mut() {
+            let mut renames = HashMap::new();
+            let fmt = |peri| format!("peripheral {peri} for device {dev_name}");
+
             for p in &mut d.peripherals {
                 if let Some(name) = match_expand(&p.name, &self.from, &self.to) {
+                    had_duplicate |=
+                        !can_rename(self.error_on_duplicate, &mut renames, &name, &p.name, fmt);
                     p.name = name;
                 }
             }
         }
+
+        if had_duplicate && self.error_on_duplicate {
+            anyhow::bail!("Failed to rename peripherals");
+        }
+
         Ok(())
     }
 }
--- src/transform/rename_registers.rs
+++ src/transform/rename_registers.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use super::common::*;
@@ -8,18 +10,34 @@ pub struct RenameRegisters {
     pub block: RegexSet,
     pub from: RegexSet,
     pub to: String,
+    #[serde(default = "get_true")]
+    pub error_on_duplicate: bool,
 }
 
 impl RenameRegisters {
     pub fn run(&self, ir: &mut IR) -> anyhow::Result<()> {
+        let mut renames = HashMap::new();
+        let mut had_duplicate = false;
+
         for id in match_all(ir.blocks.keys().cloned(), &self.block) {
             let b = ir.blocks.get_mut(&id).unwrap();
-            for i in &mut b.items {
+            let renames = renames.entry(id.clone()).or_default();
+
+            let fmt = |field| format!("register {field} in block {id}");
+
+            for i in b.items.iter_mut() {
                 if let Some(name) = match_expand(&i.name, &self.from, &self.to) {
+                    had_duplicate |=
+                        !can_rename(self.error_on_duplicate, renames, &name, &i.name, fmt);
                     i.name = name;
                 }
             }
         }
+
+        if had_duplicate && self.error_on_duplicate {
+            anyhow::bail!("Duplicate use of new names");
+        }
+
         Ok(())
     }
 }
