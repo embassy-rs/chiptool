e491b3542ffb1fdc06f7198c4a2b2e4253f79411
Alex Zepeda <github@inferiorhumanorgans.com>
2026-04-15 01:20:56 -0700

    Add MapEnumVariants transform and supporting test.
    This is useful when an SVD contains enum variants with purely numeric
    identifiers as Renesas is prone to doing.
    
    It should/could potentially be extended to use regular expressions and
    merged with the existing `RenameEnumVariants` transform. However this
    is useful in cases where something akin to a `RegexSet` might be used,
    where `RenameEnumVariants` is more useful where a small number of
    variants need to be changed.
    
    This could potentially be merged with `AddEnumVariants` but the current
    behavior is to simply ignore variants that don't match.  That allows
    a single transform to be applied to slightly different variations of
    an enum.
    
    This could potentially be merged with the `RenameFields` or more general
    `Rename` transform.

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -271,4 +271,5 @@ transforms!(
     rename_interrupts::RenameInterrupts,
     rename_peripherals::RenamePeripherals,
     clean_descriptions::CleanDescriptions,
+    map_enum_variants::MapEnumVariants,
 );
